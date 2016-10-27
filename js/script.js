d2bf5c3c8ff2e26d9afe4251b08e68c209ae7521
Tim Strecker <timstrecker97@icloud.com>
2016-10-27 08:14:29 +0200

    Slider Partner

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $('.slider-main').bxSlider({
 		pager: false,
-	});
+        auto: true
+  });
+  $('.slider-partner').bxSlider({
+		pager: false,
+        minSlides: 4,
+        maxSlides: 6,
+        moveSlides: 2,
+        slideWidth: 200
+  });
 });
