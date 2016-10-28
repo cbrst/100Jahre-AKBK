d426a3794463c1edc44df6f4d170193eb5330444
Christian Brassat <christian.brassat@gmail.com>
2016-10-28 09:02:32 +0200

    Smooth Scroll

--- js/script.js
+++ js/script.js
@@ -12,7 +12,24 @@ $(document).ready(function () {
     moveSlides: 2,
     slideWidth: 200
   });
-  
+
+  /**
+   * Smooth Scroll
+   */
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   if (window.addEventListener) {
     var state = 0,
       akbk = [65, 75, 66, 75, 49, 48, 48];
